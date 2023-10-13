5be7bf46616697afd90e0299c117bb1a419e46b0
Aymane <afirdoussi123@gmail.com>
2023-10-13 10:41:26 +0200

    pushing function to find best sigmas

--- quadratics.py
+++ quadratics.py
@@ -2,6 +2,7 @@ from optimizers import *
 from topology import *
 from dp_account import *
 from utils import *
+import math
 import pandas as pd
 import json, os, itertools
 from csv import writer
@@ -51,11 +52,19 @@ def plot_sigmacor_loss(A, B, sigma_cdp= 0.1, c_clip=1, lr=0.1, num_gossip=1, num
 
 def plot_comparison_loss(A, B, gamma, num_nodes, num_dim, sigma_cdp, sigma_cor, c_clip, num_gossip=1, num_iter = 750):
     """
-    This function plots the comparison between CDP, CD-SGD and LPD
+    This function plots the comparison between CDP, CD-SGD and LDP
     Args:
         A (array): parameter A
         B (array): parameter B
-        sigma
+        gamma (float): learning rate
+        num_nodes (int): number of nodes
+        num_dim (int): number of parameters for each worker
+        sigma_cdp (float): standard deviation of CDP noise
+        sigma_cor (float): standard deviation of correlated noise
+        c_clip (float): Gradient clip
+        num_gossip (int): gossip
+        num_iter (int): total number of iterations
+
     """
     X = np.ones(shape=(num_dim, num_nodes))
     W_ring = FixedMixingMatrix("ring", num_nodes)
@@ -86,24 +95,121 @@ def plot_comparison_loss(A, B, gamma, num_nodes, num_dim, sigma_cdp, sigma_cor, 
     folder_path = './comparison_losses'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    plt.savefig('comparison_losses/loss-n_{}-d_{}-sigmacdp_{}-sigmacor_{}.png'.format(num_nodes, num_dim, round(sigma_cdp, 2) , round(sigma_cor, 2)))
+    plt.savefig('comparison_losses/loss-n_{}-d_{}-lr_{}-clip_{}-sigmacdp_{}-sigmacor_{}-sigmaldp_{}.png'.format(num_nodes, num_dim, gamma, c_clip, round(sigma_cdp, 2) , round(sigma_cor, 2), round(sigma_ldp, 2)))
+
+
+def find_sigma_cor(sigma_cdp, sigma_cor_grid, c_clip, degree_matrix, adjacency_matrix, eps_target):
+    """
+    This function aims to find values of sigma_cor for whch we have a privacy less than eps_target
+    """
+    # median
+    n = len(sigma_cor_grid)
+    if n == 0:
+        return []
+    sigma_cor = sigma_cor_grid[ n // 2]
+    eps_end = rdp_account(sigma_cdp, sigma_cor_grid[-1], c_clip, degree_matrix, adjacency_matrix)
+    eps = rdp_account(sigma_cdp, sigma_cor, c_clip, degree_matrix, adjacency_matrix)
+    if eps_end > eps_target: # No hope, since the function is monotonous
+        return []
+    elif eps_end < eps_target: # All values are valid
+        return list(sigma_cor_grid)
+    elif abs(eps - eps_target) < 1e-4: # found
+        return [sigma_cor]
+    elif eps > eps_target: # augment sigma
+        return find_sigma_cor(sigma_cdp, sigma_cor_grid[n // 2 :], c_clip, degree_matrix, adjacency_matrix, eps_target)
+    else: #eps < eps_target
+        return sigma_cor_grid[:n // 2]
+
+
+        
+
+
+def find_best_params(A, B, gamma, num_nodes, num_dim, max_loss, target_eps, c_clip, num_gossip=1, num_iter= 1000):
+    """
+    This function searches for the best parameters sigma_cdp and sigma_cor 
+    such that they give a good performance (loss_cdp < min_loss) and a privacy under target_eps
+
+    Args:
+        A (array): parameter A
+        B (array): parameter B
+        gamma (float): learning rate
+        num_nodes (int): number of nodes
+        num_dim (int): number of parameters for each worker
+        max_loss (float): maximum loss for CDp
+        target_eps (float): maximum user-privacy
+    
+        Return:
+            sigma_cdp, sigma_cor, eps, loss_cdp (dict)
+
+    """
+    X = np.ones(shape=(num_dim, num_nodes))
+    W_ring = FixedMixingMatrix("ring", num_nodes)
+    W_centr = FixedMixingMatrix("centralized", num_nodes)
+
+    # Privacy 
+    adjacency_matrix = np.array(W_ring(0) != 0, dtype=float)
+    adjacency_matrix = adjacency_matrix - np.diag(np.diag(adjacency_matrix))
+    degree_matrix = np.diag(adjacency_matrix @ np.ones_like(adjacency_matrix[0]))
+
+    # sigma_ldp
+    sigma_ldp = c_clip * np.sqrt(2/target_eps)
+    sigma_cdp_grid = np.linspace(sigma_ldp/np.sqrt(num_nodes), sigma_ldp, 100)
+    print(f"lower bound sigma cdp {sigma_ldp/np.sqrt(num_nodes)}")
+    print(f"upper bound sigma cdp {sigma_ldp}")
+
+    # Initialization
+    eps_0 = rdp_account(sigma_cdp_grid[0], 0, c_clip, degree_matrix, adjacency_matrix)
+    errors_centr, _ = optimize_decentralized_correlated(X, W_centr, A, B, gamma, sigma_cdp_grid[0], 0, c_clip, num_gossip=num_gossip, num_iter=num_iter)
+
+    data = [{"sigma_cdp": sigma_ldp/np.sqrt(num_nodes), "sigma_cor": 0, "eps": eps_0, "loss_cdp":errors_centr[-1], "loss_cor": 10}]
+    result = pd.DataFrame(data)
+
+    # Searching
+    for sigma_cdp in sigma_cdp_grid:
+        # Looking for sigma_cor for which dp_account < target_eps
+        sigma_cor_grid = np.linspace(1, 100, 1000)
+        all_sigma_cor = find_sigma_cor(sigma_cdp, sigma_cor_grid, c_clip, degree_matrix, adjacency_matrix, target_eps)
+        
+        # Now test on the loss condition
+        if len(all_sigma_cor) != 0: # Not empty
+            for sigma_cor in all_sigma_cor:
+                errors_centr, _ = optimize_decentralized_correlated(X, W_centr, A, B, gamma, sigma_cdp, sigma_cor, c_clip, num_gossip=num_gossip, num_iter=num_iter)
+                errors_cor, _ = optimize_decentralized_correlated(X, W_ring, A, B, gamma, sigma_cdp, sigma_cor, c_clip, num_gossip=num_gossip, num_iter=num_iter)
+
+                if errors_centr[-1] <= max_loss and abs(errors_cor[-1] - result["loss_cor"].iloc[-1]) <= 5e-2:
+                    new_row = {"sigma_cdp":sigma_cdp, 
+                               "sigma_cor": sigma_cor, "eps": rdp_account(sigma_cdp, sigma_cor, c_clip, degree_matrix, adjacency_matrix),
+                                "loss_cdp": errors_centr[-1],
+                                 "loss_cor": errors_cor[-1] }
+                    result = result.append(new_row, ignore_index = True)
+
+        else:
+            continue
+    return result
+
 
 if __name__ == "__main__":
     params = {
-        "num_nodes": [30],
-        "sigma_cdp": [2, 5, 7, 10, 100],
-        "sigma_cor": np.logspace(-1, 2, 20),
+        "num_nodes": 128,
+        "sigma_cdp": [10],
+        "sigma_cor": [12],
         "num_dim": 10,
         "gamma": 0.01,
         "c_clip":1,
         "non_iid": 0
     }
+    
+    A, B = generate_functions(params["num_nodes"], params["num_dim"], params["non_iid"])
+    result = find_best_params(A, B, params["gamma"], params["num_nodes"], params["num_dim"], max_loss= 1e-2, target_eps=1e-2, c_clip=params["c_clip"],num_gossip=1, num_iter= 1000)
+    filename= "result.csv"
+    result.to_csv(filename, index=False)
 
-    for n in params["num_nodes"]:
-        A, B = generate_functions(n, params["num_dim"], params["non_iid"])
-        for sigma_cdp in params["sigma_cdp"]:
-            for sigma_cor in params["sigma_cor"]:
-                plot_comparison_loss(A, B, params["gamma"], n, params["num_dim"], sigma_cdp, sigma_cor, params["c_clip"])
+
+    #for n in params["num_nodes"]:
+    #    A, B = generate_functions(n, params["num_dim"], params["non_iid"])
+    #    for sigma_cdp in params["sigma_cdp"]:
+    #        for sigma_cor in params["sigma_cor"]:
+    #            plot_comparison_loss(A, B, params["gamma"], n, params["num_dim"], sigma_cdp, sigma_cor, params["c_clip"], num_iter=1000)
 
 # if __name__ == "__main__":
 #     base_params = {
