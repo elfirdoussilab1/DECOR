94d77dbbd09c10cf6a7d7c184c8c3e7c0156ec07
Aymane <afirdoussi123@gmail.com>
2023-11-01 16:51:57 +0100

    including more values f user-privacy

--- search_best_params.py
+++ search_best_params.py
@@ -5,18 +5,19 @@ import pandas as pd
 from utils import plotting
 
 if __name__ == "__main__":
-    params_cdp = {
-            "topology_name": "centralized",
-            "method": "CDP",
-            "gamma_grid": [4.6e-3],
+    params = {
+            "topology_name": "grid",
+            "method": "Corr",
+            "gamma_grid": [1.7e-3],
             "num_nodes": 64,
             "num_dim": 10,
-            "c_clip_grid":[1.],
+            "c_clip_grid":[1.5],
             "max_loss": 1e-2,
-            "num_iter": 3500,
+            "num_iter": 2500,
             "num_gossip": 1,
             "delta": 1e-5
         }
+    """
     params_ldp = {
             "topology_name": "ring",
             "method": "LDP",
@@ -25,7 +26,7 @@ if __name__ == "__main__":
             "num_dim": 10,
             "c_clip_grid":[1.5],
             "max_loss": 10.,
-            "num_iter": 3500,
+            "num_iter": 2500,
             "num_gossip": 1,
             "delta": 1e-5
         }
@@ -37,17 +38,17 @@ if __name__ == "__main__":
             "num_dim": 10,
             "c_clip_grid":[1.5],
             "max_loss": 1.,
-            "num_iter": 3500,
+            "num_iter": 2500,
             "num_gossip": 1,
             "delta": 1e-5
         }
-    
-    A, B = generate_functions(params_cdp["num_nodes"], params_cdp["num_dim"], zeta = 0)
+    """  
+    A, B = generate_functions(params["num_nodes"], params["num_dim"], zeta = 0)
     eps_targets = [1, 3, 5, 7, 10, 15, 20, 25, 30, 40]
-    # LDP
+
     for eps_target in eps_targets:
-        result = plotting.find_best_params(A = A, B = B, target_eps= eps_target, **params_ldp)
-        filename= f"result_grid_ldp_epsilon_{eps_target}.csv"
+        result = plotting.find_best_params(A = A, B = B, target_eps= eps_target, **params)
+        filename= f"result_gridsearch_{params['topology_name']}_{params['method']}_epsilon_{eps_target}.csv"
         result.to_csv(filename)
         df = pd.read_csv(filename)
         # Plotting results
@@ -64,28 +65,6 @@ if __name__ == "__main__":
         """
         # Plotting best result
         row = df.iloc[-1] 
-        plotting.plot_loss(row["topology"], row["method"], A, B, row["gamma"], params_ldp["num_nodes"], params_ldp["num_dim"], row['sigma'], row['sigma_cor'], row["c_clip"], 
-                           target_eps = eps_target, num_iter = params_ldp["num_iter"], delta = params_ldp["delta"])
-    # CDP
-    
-    for eps_target in eps_targets:
-        result = plotting.find_best_params(A = A, B = B, target_eps= eps_target, **params_cdp)
-        filename= f"result_grid_cdp_epsilon_{eps_target}.csv"
-        result.to_csv(filename)
-        df = pd.read_csv(filename)
-        # Plotting best results
-        row = df.iloc[-1] 
-        plotting.plot_loss(row["topology"], row["method"], A, B, row["gamma"], params_cdp["num_nodes"], params_cdp["num_dim"], row['sigma'], row['sigma_cor'], row["c_clip"], 
-                           target_eps = eps_target, num_iter = params_cdp["num_iter"], delta = params_cdp["delta"])
-        
-    # Corr
-    for eps_target in eps_targets:
-        result = plotting.find_best_params(A = A, B = B, target_eps= eps_target, **params_corr)
-        filename= f"result_grid_corr_epsilon_{eps_target}.csv"
-        result.to_csv(filename)
-        df = pd.read_csv(filename)
-        # Plotting best result
-        row = df.iloc[-1] 
-        plotting.plot_loss(row["topology"], row["method"], A, B, row["gamma"], params_corr["num_nodes"], params_corr["num_dim"], row['sigma'], row['sigma_cor'], row["c_clip"], 
-                           target_eps = eps_target, num_iter = params_corr["num_iter"], delta = params_corr["delta"])
+        plotting.plot_loss(row["topology"], row["method"], A, B, row["gamma"], params["num_nodes"], params["num_dim"], row['sigma'], row['sigma_cor'], row["c_clip"], 
+                           target_eps = eps_target, num_iter = params["num_iter"], delta = params["delta"])
         
